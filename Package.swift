215f14433e84cc96023fa7e78370a9fa525e9215
Shingo Hiraya <shingohiraya@gmail.com>
2017-03-13 15:43:57 +0900

    first commit

---
+++ Package.swift
@@ -0,0 +1,16 @@
+import PackageDescription
+
+let package = Package(
+    name: "vapor-multipart-formdata-sample",
+    dependencies: [
+        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 5)
+    ],
+    exclude: [
+        "Config",
+        "Database",
+        "Localization",
+        "Public",
+        "Resources",
+    ]
+)
+
