2b02632d7fae1d497ad8a28f4e242cf6fb5d97e6
Shingo Hiraya <shingohiraya@gmail.com>
2017-03-13 18:14:53 +0900

    add POST handler

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -1,4 +1,5 @@
 import Vapor
+import Foundation
 
 let drop = Droplet()
 
@@ -6,6 +7,21 @@ drop.get { req in
     return try drop.view.make("welcome", [
     	"message": drop.localization[req.lang, "welcome", "title"]
     ])
+}
+
+drop.post { req in
+    guard let field = req.formData?["file"] else {
+        return JSON(["result":"receive failure"])
+    }
+    
+    do {
+        let dest = URL(fileURLWithPath: field.filename!)
+        try Data(field.part.body).write(to: dest)
+    } catch {
+        return JSON(["result":"save failure"])
+    }
+    
+    return JSON(["result":"success"])
 }
 
 drop.resource("posts", PostController())
